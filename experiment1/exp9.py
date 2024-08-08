766333f3a6501e5dc5d884c92b34dd61d06f4f76
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-08 23:17:23 +0530

    fixed 9th one

--- experiment1/exp9.py
+++ experiment1/exp9.py
@@ -5,7 +5,7 @@ y = float(input("the value of y: "))
 n = float(input("the value of n: "))
 
 if (n>=2 and n<=8) :
-    result = ((((x**2)+(x**2))/((y/4)+(y/3)+(y**8)))**(2*n))*(((y**6)+(y**2))/(x**9))
+    result = ((((x**2)+(x**3))/((y/4)+(y/3)+(y**8)))**(2*n))*(((y**6)+(y**2))/(x**9))
     print(result)
 else :
     print("invalid value of n")
