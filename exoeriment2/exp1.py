db090c0906c27451c8ee7fb2d821cf4fb480ecae
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-11 20:20:28 +0530

    exp1

---
+++ exoeriment2/exp1.py
@@ -0,0 +1,18 @@
+# Complex number
+
+import cmath
+
+x1 = float(input("First Real part: "))
+y1 = float(input("First complex part: "))
+x2 = float(input("Second Real part: "))
+y2 = float(input("Second complex part: "))
+
+z1 = complex(x1,y1)
+z2 = complex(x2,y2)
+
+addition = z1 + z2
+ 
+subtraction = z1 - z2
+
+print("After addition the complex number is ", addition)
+print("After subtraction the complex number is ", subtraction)
