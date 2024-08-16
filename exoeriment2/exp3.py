ecf14735b3aa375fe234879bbef51c9bea260d99
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-16 11:08:51 +0530

    initial exp3

--- exoeriment2/exp3.py
+++ exoeriment2/exp3.py
@@ -3,4 +3,4 @@ Name = str(input("Input your name: "))
 Name = Name[::-1]
 
 print("reversed name is: ", Name)
-print("reversed 5 words of name are: ", Name[:5:-1])
+print("reversed 5 words of name are: ", Name[-5:])
---
+++ experiment3/practice.py
@@ -0,0 +1,25 @@
+# Number = int(input("Give any number: "))
+
+# if (Number%2 == 0) :
+#     print("yes")
+
+# elif (Number%3 == 0) :
+#     print()
+
+# else :
+#     print("no")
+
+# Number = int(input("Give any number: "))
+
+# if (Number%2==0 or Number%3==0) :
+#     print("yes")
+# else:
+#     print("no")
+
+Age = int(input("Input age: "))
+
+if Age>18:
+    if Age < 65:
+        print("eligible")
+    else :
+        print("you are too old")
