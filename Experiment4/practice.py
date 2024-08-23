91c12319ab26bd327575becc18a047a30e80d1d7
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-23 10:33:35 +0530

    Experiment4 initialization

---
+++ Experiment4/practice.py
@@ -0,0 +1,9 @@
+String = "we know what we are"
+# String = String.replace("hello", "World")
+print(String.upper())
+print(String.lower())
+print(String.title())
+print(String.capitalize())
+print(String.swapcase())
+# print(String.index("w"))
+print(String.index("w",String.index("w",String.index("w")+1)+1))
