651053eb58fa23491717ed63afbbd908b95d2090
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-30 21:06:43 +0530

    Practiced in class

---
+++ experiment5/practice.py
@@ -0,0 +1,64 @@
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i,"X",j,"=", i*j)
+    
+# for i in range(50, 101):
+#     if i%2==0:
+#         print(i)
+
+# list1=[2,4,6,8,10]
+
+# for i in list1:
+#     print(i)
+
+# i = 1
+# while i<=list1[-1]:
+#     if i in list1:
+#         print(i)
+#     i+=1
+
+# n = int(input("Take a number: "))
+
+# i=1
+
+# while i<len(list1):
+#     if list1[i]==n:
+#         print("yes")
+#         break
+#     elif list1[i]==list1[-1]:
+#         print("no")
+#     i+=1
+
+# list1=[]
+# i=0
+# while i<5:
+#     list1.append(int(input("Input a number: ")))
+#     i+=1
+
+# print(list1)
+# list2=[]
+# for i in reversed(list1):
+#     list2.append(i)
+
+# print(list2)
+
+
+# for i in list1:
+#     for j in range(1,11):
+#         print(i,"X",j,"=", i*j)
+
+# crush_list=["Hello", "world", "Me", "Myself"]
+
+# for i in range(len(crush_list)):
+#     print("gf", i+1,":",crush_list[i])
+
+list1=[1,2,3,4,5]
+
+for i in list1:
+    print(i,"X 3 =",i*3)
