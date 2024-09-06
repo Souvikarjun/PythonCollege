47b76d9d59bae5c8617bc05acd9fca5730e576ac
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-09-06 21:16:30 +0530

    practice 6

--- Experiment6/practice.py
+++ Experiment6/practice.py
@@ -1,1 +1,18 @@
 #for practice purpose only
+
+# def square():
+#     for i in range(0,7):
+#         print("*******")
+
+def area(hight, width=2):
+    area = hight*width
+    return area
+
+def main():
+    hight = float(input("inout hight: "))
+    width = float(input("inout width: "))
+    Area = area(hight, width)
+    print(Area)
+
+if __name__=='__main__':
+    main()
